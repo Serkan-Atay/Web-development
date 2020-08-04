b06c82f741d3b4128060efe1eeb681108bb88fca
Serkan <45064845+Serkan-Atay@users.noreply.github.com>
2020-08-04 22:51:23 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+// This is my first JavaScript code!
+console.log("Hello World");
+
+let name = "222janjna an dkan a kanfkjaf/hf\nenn\njndsfjdf";
+console.log(name);
+
+let firstName = 'Serkan';
+let lastName = 'Atay';
+
+console.log(firstName + ' ' + lastName);
